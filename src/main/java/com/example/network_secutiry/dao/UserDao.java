2bcd9c4a92d85542f9dc9c785967130030c789a2
hoangsonhv <hvsson102@gmail.com>
2022-08-19 09:58:25 +0700

    Network Security

--- src/main/java/com/example/network_secutiry/dao/UserDao.java
+++ src/main/java/com/example/network_secutiry/dao/UserDao.java
@@ -4,32 +4,25 @@ package com.example.network_secutiry.dao;
 import com.example.network_secutiry.model.User;
 import com.example.network_secutiry.utils.ConnectionUtils;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UserDao {
     Connection connection = ConnectionUtils.getConnection();
 
     public User getUser(String pName, String password) {
         User user = null;
-//        String sql = "select * from users where username = '"+pName +"' and password = " +password;
-        String sql = "select * from users where username = ? and password = ?";
         try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setString(1, pName);
-            ps.setString(2, password);
-            ResultSet rs = ps.executeQuery();
+            Statement statement = connection.createStatement();
+            String sqlStatement = String.format("select * from users where username = '"+pName +"' and password = " +password);
+            ResultSet rs = statement.executeQuery(sqlStatement);
             while (rs.next()){
                 String name = rs.getString("username");
                 String pass = rs.getString("password");
                 user = new User(name, pass);
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        }catch(SQLException e){
+            e.printStackTrace();
         }
         return user;
     }
-
 }
